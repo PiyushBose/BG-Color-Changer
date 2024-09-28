c800d7f8a8208f6f95cda2514734a6b1d764e77e
PiyushBose <piyushbose05@gmail.com>
2024-09-28 10:37:55 +0530

    Custom functionality added

--- script.js
+++ script.js
@@ -5,12 +5,47 @@ let colors = ["red", "green", "blue"];
 function createPopUp(){
     document.querySelector(".pop-up").classList.remove("hide");
     document.querySelector(".container").classList.add("blur");
+    const input = document.querySelector("input");
+    input.focus();
+    input.select();
+    input.value = '';
+    
+    input.addEventListener("keyup", (e)=>{
+        if(e.key === "Enter"){
+            e.stopImmediatePropagation();
+            e.preventDefault();
+            addColor();
+        }
+    })
+
+    input.addEventListener("keyup", (e)=>{
+        if(e.key === "Escape"){
+            e.preventDefault();
+            closePopUp();
+        }
+    })
+    
 }
 
 function closePopUp(){
     document.querySelector(".pop-up").classList.add("hide");
     document.querySelector(".container").classList.remove("blur");
 
+}
+
+function addColor(){
+    const inputColor = document.querySelector("input").value;
+    if(CSS.supports('color', inputColor)){
+        colors.push(inputColor);
+        closePopUp();
+        render();
+    }
+    else{
+        const invalid = document.createElement("div");
+        invalid.innerHTML = 'Invalid Color! Try something else';
+        invalid.classList.add("invalid");
+        document.querySelector("input").after(invalid);
+    }
 }
 
 function updateColor(color){
